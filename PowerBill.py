ac897e03fcb88ae5aa634a7b69a4a1aef1f78c3f
Ayush Singh <ayush893singh@gmail.com>
2026-06-11 19:51:21 +0530

    Add files via upload

---
+++ PowerBill.py
@@ -0,0 +1,14 @@
+print("===== Electricity Bill Calculator =====")
+
+units = float(input("Enter electricity units consumed: "))
+
+if units <= 100:
+    bill = units * 5
+
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+
+else:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+
+print(f"\nTotal Bill Amount = ₹{bill}")
